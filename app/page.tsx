13b6ec4396afa230b0f54844acdc7b29604297c2
xMarioOP <marioalejandro424@outlook.com>
2024-11-05 20:43:43 -0500

    Working with non-typed libraries

--- app/page.tsx
+++ app/page.tsx
@@ -3,8 +3,11 @@
 import { MouseEventHandler, } from "react";
 import { useState } from "react";
 import { LazyImage } from "./components/RandomFox";
+import { random } from "lodash"
 
-const random = () => Math.floor(Math.random() * 124) + 1
+const myRandom = () => random(1, 123)
+
+
 const generateId = () => Math.random().toString(36).substring(2, 9)
 
 export default function Home() {
@@ -13,7 +16,7 @@ export default function Home() {
   const addNewFox: MouseEventHandler<HTMLButtonElement> = (e) => {
     e.preventDefault()
 
-    const newImageItem: IFoxImageItem = { id: generateId(), url: `https://randomfox.ca/images/${random()}.jpg` }
+    const newImageItem: IFoxImageItem = { id: generateId(), url: `https://randomfox.ca/images/${myRandom()}.jpg` }
     setImages([
       ...images,
       newImageItem
