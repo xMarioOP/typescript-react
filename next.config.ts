55ded239fed9e5279802d9d22d326d0a4876b8f9
xMarioOP <marioalejandro424@outlook.com>
2024-11-06 19:02:06 -0500

    Prepare project for deployment to GitHub Pages: configure basePath, update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -2,9 +2,9 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  output: 'export',
   basePath: '/typescript-react',
   assetPrefix: '/typescript-react',
-  output: 'export',
 };
 
 export default nextConfig;
